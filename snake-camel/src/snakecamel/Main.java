4acaa3ac7303a1fc91dffb32c3e70933e1558996
g13910ia <g13910ia@gm.tsuda.ac.jp>
2015-05-01 12:08:27 +0900

    capitalizeの確認をした

---
+++ snake-camel/src/snakecamel/Main.java
@@ -0,0 +1,12 @@
+package snakecamel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(SnakeCamelUtil.capitalize(" "));
+		System.out.println(SnakeCamelUtil.capitalize("a"));
+		System.out.println(SnakeCamelUtil.capitalize("xyz"));
+	}
+
+}
